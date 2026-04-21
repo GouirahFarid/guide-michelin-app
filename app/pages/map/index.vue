8ad28ffaa0c6d63f079e057b788b03df3a956339
gouirah <gouirahfarid@gmail.com>
2026-04-21 15:00:54 +0200

    add mapbox

---
+++ app/pages/map/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <MapboxMap
+      map-id="hello-aaa"
+      style="position: absolute; top: 0; bottom: 0; width: 100%"
+      :options="{
+        style: 'mapbox://styles/mapbox/dark-v11', // style URL
+        center: [2.2137, 46.2276], // starting position
+        zoom: 5 // starting zoom
+      }"
+  />
+
+</template>
+
+<style scoped>
+
+</style>
